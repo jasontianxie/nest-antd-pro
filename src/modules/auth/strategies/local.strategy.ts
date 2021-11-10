e70133ed4769014d382229c300ca7f31a1229d0c
duxing <duxing@qunhemail.com>
2021-11-10 16:01:58 +0800

    feat: 登录失败的信息

--- src/modules/auth/strategies/local.strategy.ts
+++ src/modules/auth/strategies/local.strategy.ts
@@ -1,6 +1,6 @@
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { AuthService } from '../auth.service';
 
 @Injectable()
@@ -12,7 +12,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(username: string, password: string): Promise<any> { // 登录的时候使用手机号码作为username
     const user = await this.authService.validateUser(username, password);
     if (!user) {
-      throw new UnauthorizedException();
+      throw new HttpException('登录失败', HttpStatus.OK);
     }
     return user;
   }
